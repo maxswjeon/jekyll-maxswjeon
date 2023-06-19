e7b70fbe92538178e9d381a3ccfc4ca03ce26fee
Sangwan Jeon <commits@swjeon.kr>
2023-06-19 15:10:33 +0000

    fix: handler on wrong element

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,9 @@
-document.body.addEventListener("click", function () {
-  if (document.body.hasAttribute("data-sidebar-open")) {
-    document.body.removeAttribute("data-sidebar-open");
-  } else {
-    document.body.setAttribute("data-sidebar-open", "");
-  }
-});
+document
+  .getElementById("sidebar-toggle")
+  .addEventListener("click", function () {
+    if (document.body.hasAttribute("data-sidebar-open")) {
+      document.body.removeAttribute("data-sidebar-open");
+    } else {
+      document.body.setAttribute("data-sidebar-open", "");
+    }
+  });
